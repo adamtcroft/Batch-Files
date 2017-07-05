da60009df8c35ed7b04b7612df974273358e1776
Adam T. Croft <sendtoatc@gmail.com>
2017-07-05 00:39:29 -0700

    Adding ls.bat
    ls.bat cleanly lists the contents of a directory

---
+++ ls.bat
@@ -0,0 +1,12 @@
+:: ls.bat
+:: A short batch file to
+:: quickly and cleanly list
+:: a directory's contents
+::
+:: Adam T. Croft -- 2017
+:: adamtcroft.com
+
+:: Turn off command echo
+@ECHO OFF
+
+dir /b /a-d
