1fd2787876bbede86e5133c826317e8fa9e863a2
Adam T. Croft <sendtoatc@gmail.com>
2017-07-06 00:15:57 -0700

    added google.bat

--- google.bat
+++ google.bat
@@ -7,3 +7,20 @@
 
 :: Turn off command echo
 @ECHO OFF
+
+if %1 == -f goto :firefox
+if %1 == -c goto :chrome
+goto :default
+
+:firefox
+firefox https://www.google.com/search?q="%~2"
+goto :end
+
+:chrome
+chrome https://www.google.com/search?q="%~2"
+goto :end
+
+:default
+start https://www.google.com/search?q="%~1"
+
+:end
