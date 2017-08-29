c525e4ece0200d1e35d855f115c81a957bcd575c
Adam T. Croft <sendtoatc@gmail.com>
2017-08-28 19:10:49 -0700

    Adding cycleIP.bat

---
+++ cycleIP.bat
@@ -0,0 +1,12 @@
+:: gmail.bat
+:: A short batch file to
+:: jump right to gmail
+::
+:: Adam T. Croft -- 2017
+:: adamtcroft.com
+
+:: Turn off command echo
+@ECHO OFF
+
+ipconfig /release
+ipconfig /renew
